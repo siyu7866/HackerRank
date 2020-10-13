2013a8ca3b69b3f55c4a4d5f4542af8a300471a2
Siyu Xiang <siyuxiang@SiyudeMacBook-Air.local>
2020-10-13 02:07:48 -0700

    SherlockAndAnagrams with HashMap

---
+++ SherlockAndAnagrams/SherlockAndAnagrams.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class SherlockAndAnagrams {
+
+    // Complete the sherlockAndAnagrams function below.
+    static int sherlockAndAnagrams(String s) {
+        HashMap<String, Integer> map = new HashMap<>();
+        //char[] array = s.toCharArray();
+        int res = 0;
+        for(int i = 0; i < s.length(); i++) {
+            for(int j = 1; j < s.length(); j++) {
+                if(i + j <= s.length()) {
+                    //String newS = new String(array)
+                    String newS = sortString(s.substring(i, i + j));
+                    if(!map.containsKey(newS)) {
+                        map.put(newS, 1);
+                    } else {
+                        int tmp = map.get(newS);
+                        tmp++;
+                        map.put(newS, tmp);
+                        res = res + tmp - 1;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
